0d3976d5d3af2d175738739d0699356e899b82dc
이하린 <harin4157413@gmail.com>
2025-09-17 01:05:17 +0900

    five

--- src/c202314056/Main.java
+++ src/c202314056/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("이하린");  
+        System.out.println("홍길동");
         World.run();
+        World.run2();
         Hello.run();
     }
 }
--- src/c202314056/World.java
+++ src/c202314056/World.java
@@ -1,5 +1,8 @@
-package c202314056;
-
 public class World {
-
+	public static void run() {
+		System.out.println("202314056");
+	}
+	public static void run2() {
+		System.out.println("hello world")
+	}
 }
